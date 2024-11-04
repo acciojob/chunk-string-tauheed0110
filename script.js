d58dd255881e9f1198da5e68d25066f4a7f888f1
tauheed0110 <tauheed0110@gmail.com>
2024-11-04 15:41:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function stringChop(str, size) {
-  // your code here
+    const arr = [];
+    while(str){
+        if(str.length < size){
+            arr.push(str);
+            str = "";
+        }else{
+            arr.push(str.slice(0, size))
+            str = str.slice(size);
+        }
+       
+    }
+    return arr;
 }
 
 // Do not change the code below
